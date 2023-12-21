6f5ac0eb128040304b4a3b5f3651e6aba495bbeb
Thien Phuc <thienphuc484@gmail.com>
2023-12-21 11:40:30 +0700

    Update LoginController

--- src/main/java/com/Blog/Controller/LoginController.java
+++ src/main/java/com/Blog/Controller/LoginController.java
@@ -10,10 +10,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -27,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> authUser(@RequestBody LoginDTO loginDTO){
+    public ResponseEntity<Object> authUser(@RequestBody LoginDTO loginDTO){
         if (StringUtils.isEmpty(loginDTO.getEmail()) || StringUtils.isEmpty(loginDTO.getPassword())) {
             return new ResponseEntity<>("Email and password cannot be empty.", HttpStatus.BAD_REQUEST);
         }
@@ -37,7 +37,10 @@ public class LoginController {
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
             String token = jwtService.generateToken(loginDTO.getEmail());
-            return ResponseEntity.ok(token);
+            Map<String, String> response = new HashMap<>();
+            response.put("token", token);
+
+            return ResponseEntity.ok(response);
         }catch (Exception e){
             return new ResponseEntity<>("Invalid email or password.", HttpStatus.UNAUTHORIZED);
         }
