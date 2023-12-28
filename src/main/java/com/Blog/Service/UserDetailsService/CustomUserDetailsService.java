bdb5e0b3cf8d74a908970877b26bb18336bcd2fc
Thien Phuc <thienphuc484@gmail.com>
2023-12-28 12:24:25 +0700

    Update CustomUserDetailsService. Add Logging.

--- src/main/java/com/Blog/Service/UserDetailsService/CustomUserDetailsService.java
+++ src/main/java/com/Blog/Service/UserDetailsService/CustomUserDetailsService.java
@@ -6,6 +6,8 @@ import com.Blog.Model.User;
 import com.Blog.Repository.RoleRepository;
 import com.Blog.Repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -23,44 +25,57 @@ import java.util.List;
 @Transactional
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired private UserRepository UserRepository;
-    @Autowired private RoleRepository roleRepository;
+    private static final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = UserRepository.findUserByUsername(username);
-        if(user == null){
+        logger.info("Loading user by username: {}", username);
+
+        User user = userRepository.findUserByUsername(username);
+        if (user == null) {
+            logger.warn("User not found with username: {}", username);
             return new org.springframework.security.core.userdetails.User(
-                    " "," ",true,true,true,true,
+                    " ", " ", true, true, true, true,
                     getAuthorities(Arrays.asList(roleRepository.findByName("ROLE_USER")))
             );
         }
+
+        logger.info("Found user: {}", user.getUsername());
+
         return new org.springframework.security.core.userdetails.User(
-                user.getEmail(),user.getPassword(),true,true,true,true
-                ,getAuthorities(user.getRoles())
+                user.getEmail(), user.getPassword(), true, true, true, true,
+                getAuthorities(user.getRoles())
         );
     }
 
-    private Collection<? extends GrantedAuthority> getAuthorities(Collection<Role> roles){
+    private Collection<? extends GrantedAuthority> getAuthorities(Collection<Role> roles) {
         return getGrantedAuthorities(getPrivileges(roles));
     }
 
-    private List<String> getPrivileges(Collection<Role> roles){
+    private List<String> getPrivileges(Collection<Role> roles) {
         List<String> privileges = new ArrayList<>();
         List<Privilege> collection = new ArrayList<>();
-        for(Role role : roles){
+        for (Role role : roles) {
             privileges.add(role.getName());
             collection.addAll(role.getPrivileges());
         }
-        for(Privilege item : collection){
+        for (Privilege item : collection) {
             privileges.add(item.getName());
         }
+        logger.info("Privileges: {}", privileges);
         return privileges;
     }
 
-    private List<GrantedAuthority> getGrantedAuthorities(List<String> privileges){
+    private List<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
+        logger.info("Getting granted authorities for privileges: {}", privileges);
         List<GrantedAuthority> authorities = new ArrayList<>();
-        for(String privilege : privileges){
+        for (String privilege : privileges) {
             authorities.add(new SimpleGrantedAuthority(privilege));
         }
         return authorities;
